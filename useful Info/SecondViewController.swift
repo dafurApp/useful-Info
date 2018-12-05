daba2552a86c1c8cffdc958d36f5f7618b9fae1e
Daniel Furrer <ddfurrer@bluemail.ch>
2018-12-05 23:41:59 +0100

    Initial Commit

---
+++ useful Info/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  useful Info
+//
+//  Created by Daniel Furrer on 05.12.18.
+//  Copyright © 2018 Daniel Furrer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
